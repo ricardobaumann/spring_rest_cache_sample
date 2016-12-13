72fb6a8e4d389b8da62acd52a461f4879ac48d39
ricardobaumann <ricardo.luis.baumann@gmail.com>
2016-12-13 18:15:20 +0100

    Project creation

---
+++ src/main/java/com/github/ricardobaumann/actuator/SampleController.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2012-2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.ricardobaumann.actuator;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.context.annotation.Description;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.Collections;
+import java.util.Map;
+
+@Controller
+@Description("A controller for handling requests for hello messages")
+public class SampleController {
+
+	@Autowired
+	private HelloWorldService helloWorldService;
+
+	@RequestMapping("/")
+	@ResponseBody
+	@Cacheable("helloWorld")
+	public Map<String, String> helloWorld() {
+
+        System.out.println("hitting method");
+
+		return Collections.singletonMap("message",
+				this.helloWorldService.getHelloMessage());
+	}
+
+	@RequestMapping("/foo")
+	@ResponseBody
+	public String foo() {
+		throw new IllegalArgumentException("Server error");
+	}
+
+	@RequestMapping("clear")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @CacheEvict("helloWorld")
+	public void clearCache() {
+
+    }
+}
---
+++ start.groovy
@@ -0,0 +1,5 @@
+@Grab("com.github.ricardobaumann:spring-boot-starter-actuator:0.5.0.BUILD-SNAPSHOT")
+@Grab("com.github.ricardobaumann:spring-boot-starter-web:0.5.0.BUILD-SNAPSHOT")
+@ComponentScan("com.github.ricardobaumann.sample.service")
+class Start {
+}
